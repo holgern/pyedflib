4f25949679c4a3179a6111c48aedfce15ff85ef6
Holger Nahrstaedt <holgernahrstaedt@gmx.de>
2015-10-18 13:15:51 +0200

    init file added

---
+++ pyedflib/tests/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import division, print_function, absolute_import
+
+from .edfwriter import *
+from .edfreader import *
+from ._edflib import *
+
+__all__ = [s for s in dir() if not s.startswith('_')]
+
+from pyedflib.version import version as __version__
