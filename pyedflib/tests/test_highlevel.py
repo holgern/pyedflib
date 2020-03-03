4490774a47f37c3614b8fd3e9ae2740efe9e02d4
skjerns <14980558+skjerns@users.noreply.github.com>
2020-03-03 13:57:14 +0100

    re-enable other test

--- pyedflib/tests/test_highlevel.py
+++ pyedflib/tests/test_highlevel.py
@@ -34,50 +34,50 @@ class TestHighLevel(unittest.TestCase):
         signals_dig2 = np.rint(signals_dig2)
         np.testing.assert_allclose(signals_dig, signals_dig2)
 
-    # def test_read_write_edf(self):
-    #     startdate = datetime.now()
-    #     t = startdate
-    #     startdate = datetime(t.year,t.month,t.day,t.hour, t.minute,t.second)
+    def test_read_write_edf(self):
+        startdate = datetime.now()
+        t = startdate
+        startdate = datetime(t.year,t.month,t.day,t.hour, t.minute,t.second)
         
-    #     header = highlevel.make_header(technician='tech', recording_additional='radd',
-    #                                             patientname='name', patient_additional='padd',
-    #                                             patientcode='42', equipment='eeg', admincode='420',
-    #                                             gender='Male', startdate=startdate,birthdate='05.09.1980')
-    #     annotations = [[0.01, -1, 'begin'],[0.5, -1, 'middle'],[10, -1, 'end']]
-    #     header['annotations'] = annotations
-    #     signal_headers1 = highlevel.make_signal_headers(['ch'+str(i) for i in range(5)])
-    #     signals = np.random.rand(5, 256*300)*200 #5 minutes of eeg
+        header = highlevel.make_header(technician='tech', recording_additional='radd',
+                                                patientname='name', patient_additional='padd',
+                                                patientcode='42', equipment='eeg', admincode='420',
+                                                gender='Male', startdate=startdate,birthdate='05.09.1980')
+        annotations = [[0.01, -1, 'begin'],[0.5, -1, 'middle'],[10, -1, 'end']]
+        header['annotations'] = annotations
+        signal_headers1 = highlevel.make_signal_headers(['ch'+str(i) for i in range(5)])
+        signals = np.random.rand(5, 256*300)*200 #5 minutes of eeg
         
-    #     success = highlevel.write_edf(self.edfplus_data_file, signals, signal_headers1, header)
-    #     self.assertTrue(os.path.isfile(self.edfplus_data_file))
-    #     self.assertGreater(os.path.getsize(self.edfplus_data_file), 0)
-    #     self.assertTrue(success)
+        success = highlevel.write_edf(self.edfplus_data_file, signals, signal_headers1, header)
+        self.assertTrue(os.path.isfile(self.edfplus_data_file))
+        self.assertGreater(os.path.getsize(self.edfplus_data_file), 0)
+        self.assertTrue(success)
         
-    #     signals2, signal_headers2, header2 = highlevel.read_edf(self.edfplus_data_file)
+        signals2, signal_headers2, header2 = highlevel.read_edf(self.edfplus_data_file)
 
-    #     self.assertEqual(len(signals2), 5)
-    #     self.assertEqual(len(signals2), len(signal_headers2))
-    #     for shead1, shead2 in zip(signal_headers1, signal_headers2):
-    #         self.assertDictEqual(shead1, shead2)
+        self.assertEqual(len(signals2), 5)
+        self.assertEqual(len(signals2), len(signal_headers2))
+        for shead1, shead2 in zip(signal_headers1, signal_headers2):
+            self.assertDictEqual(shead1, shead2)
             
-    #     self.assertDictEqual(header, header2)
-    #     np.testing.assert_allclose(signals, signals2, atol=0.01)
+        self.assertDictEqual(header, header2)
+        np.testing.assert_allclose(signals, signals2, atol=0.01)
     
-    #     signals = (signals*100).astype(np.int8)
-    #     success = highlevel.write_edf(self.edfplus_data_file, signals,  signal_headers1, header, digital=True)
-    #     self.assertTrue(os.path.isfile(self.edfplus_data_file))
-    #     self.assertGreater(os.path.getsize(self.edfplus_data_file), 0)
-    #     self.assertTrue(success)
+        signals = (signals*100).astype(np.int8)
+        success = highlevel.write_edf(self.edfplus_data_file, signals,  signal_headers1, header, digital=True)
+        self.assertTrue(os.path.isfile(self.edfplus_data_file))
+        self.assertGreater(os.path.getsize(self.edfplus_data_file), 0)
+        self.assertTrue(success)
         
-    #     signals2, signal_headers2, header2 = highlevel.read_edf(self.edfplus_data_file, digital=True)
+        signals2, signal_headers2, header2 = highlevel.read_edf(self.edfplus_data_file, digital=True)
 
-    #     self.assertEqual(len(signals2), 5)
-    #     self.assertEqual(len(signals2), len(signal_headers2))
-    #     for shead1, shead2 in zip(signal_headers1, signal_headers2):
-    #         self.assertDictEqual(shead1, shead2)
+        self.assertEqual(len(signals2), 5)
+        self.assertEqual(len(signals2), len(signal_headers2))
+        for shead1, shead2 in zip(signal_headers1, signal_headers2):
+            self.assertDictEqual(shead1, shead2)
             
-    #     self.assertDictEqual(header, header2)
-    #     np.testing.assert_array_equal(signals, signals2)
+        self.assertDictEqual(header, header2)
+        np.testing.assert_array_equal(signals, signals2)
         
         
 
