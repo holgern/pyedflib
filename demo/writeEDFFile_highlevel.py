b16010327921524ef670f7a53cc68e06f8fe2b1b
guiCat <guillaume.cathelain@gmail.com>
2021-08-26 11:28:36 +0200

    Writing decimal sample rated signals using high level libraries

---
+++ demo/writeEDFFile_highlevel.py
@@ -0,0 +1,124 @@
+from __future__ import division, print_function, absolute_import
+
+import os
+import numpy as np
+from scipy import signal
+import pyedflib
+
+# signal label/waveform  amplitude    f       sf
+# ---------------------------------------------------
+#    1    squarewave        100 uV    0.1Hz   200 Hz
+#    2    ramp              100 uV    1 Hz    200 Hz
+#    3    pulse 1           100 uV    1 Hz    200 Hz
+#    4    pulse 2           100 uV    1 Hz    256 Hz
+#    5    pulse 3           100 uV    1 Hz    217 Hz
+#    6    noise             100 uV    - Hz    200 Hz
+#    7    sine 1 Hz         100 uV    1 Hz    200 Hz
+#    8    sine 8 Hz         100 uV    8 Hz    200 Hz
+#    9    sine 8.1777 Hz    100 uV    8.25 Hz 200 Hz
+#    10    sine 8.5 Hz       100 uV    8.5Hz   200 Hz
+#    11    sine 15 Hz        100 uV   15 Hz    200 Hz
+#    12    sine 17 Hz        100 uV   17 Hz    200 Hz
+#    13    sine 50 Hz        100 uV   50 Hz    200 Hz
+
+
+if __name__ == '__main__':
+    test_data_file = os.path.join('.', 'test_generator2.edf')
+    file_duration = 600
+    block_size = -1
+
+    signal_headers = []
+    signals = []
+
+    ch_dict = {'label': 'squarewave', 'dimension': 'uV', 'sample_rate': 200, 'physical_max': 100,
+               'physical_min': -100, 'digital_max': 32767, 'digital_min': -32768, 'transducer': '', 'prefilter': ''}
+    signal_headers.append(ch_dict)
+    time = np.linspace(0, file_duration, file_duration * 200)
+    xtemp = np.sin(2 * np.pi * 0.1 * time)
+    x1 = xtemp.copy()
+    x1[np.where(xtemp > 0)[0]] = 100
+    x1[np.where(xtemp < 0)[0]] = -100
+    signals.append(x1)
+
+    ch_dict = {'label': 'ramp', 'dimension': 'uV', 'sample_rate': 200, 'physical_max': 100, 'physical_min': -100,
+               'digital_max': 32767, 'digital_min': -32768, 'transducer': '', 'prefilter': ''}
+    signal_headers.append(ch_dict)
+    time = np.linspace(0, file_duration, file_duration * 200)
+    x2 = signal.sawtooth(2 * np.pi * 1 * time)
+    signals.append(x2)
+
+    ch_dict = {'label': 'pulse 1', 'dimension': 'uV', 'sample_rate': 200, 'physical_max': 100,
+               'physical_min': -100, 'digital_max': 32767, 'digital_min': -32768, 'transducer': '', 'prefilter': ''}
+    signal_headers.append(ch_dict)
+    time = np.linspace(0, file_duration, file_duration * 200)
+    xtemp = np.sin(2 * np.pi * 0.5 * time)
+    x3 = np.zeros(file_duration * 200)
+    x3[np.where(np.all((xtemp < 0.02, xtemp > -0.02), axis=0))[0]] = 100
+    signals.append(x3)
+
+    ch_dict = {'label': 'pulse 2', 'dimension': 'uV', 'sample_rate': 256, 'physical_max': 100,
+               'physical_min': -100, 'digital_max': 32767, 'digital_min': -32768, 'transducer': '', 'prefilter': ''}
+    signal_headers.append(ch_dict)
+    time = np.linspace(0, file_duration, file_duration * 256)
+    xtemp = np.sin(2 * np.pi * 0.5 * time)
+    x4 = np.zeros(file_duration * 256)
+    x4[np.where(np.all((xtemp < 0.02, xtemp > -0.02), axis=0))[0]] = 100
+    signals.append(x4)
+
+    ch_dict = {'label': 'pulse 3', 'dimension': 'uV', 'sample_rate': 217, 'physical_max': 100,
+               'physical_min': -100, 'digital_max': 32767, 'digital_min': -32768, 'transducer': '', 'prefilter': ''}
+    signal_headers.append(ch_dict)
+    time = np.linspace(0, file_duration, file_duration * 217)
+    xtemp = np.sin(2 * np.pi * 0.5 * time)
+    x5 = np.zeros(file_duration * 217)
+    x5[np.where(np.all((xtemp < 0.02, xtemp > -0.02), axis=0))[0]] = 100
+    signals.append(x5)
+
+    ch_dict = {'label': 'noise', 'dimension': 'uV', 'sample_rate': 200, 'physical_max': 100, 'physical_min': -100,
+               'digital_max': 32767, 'digital_min': -32768, 'transducer': '', 'prefilter': ''}
+    signal_headers.append(ch_dict)
+    signals.append(np.random.normal(size=file_duration * 200))
+
+    ch_dict = {'label': 'sine 1 Hz', 'dimension': 'uV', 'sample_rate': 200, 'physical_max': 100,
+               'physical_min': -100, 'digital_max': 32767, 'digital_min': -32768, 'transducer': '', 'prefilter': ''}
+    signal_headers.append(ch_dict)
+    time = np.linspace(0, file_duration, file_duration * 200)
+    signals.append(np.sin(2 * np.pi * 1 * time))
+
+    ch_dict = {'label': 'sine 8 Hz', 'dimension': 'uV', 'sample_rate': 200, 'physical_max': 100,
+               'physical_min': -100, 'digital_max': 32767, 'digital_min': -32768, 'transducer': '', 'prefilter': ''}
+    signal_headers.append(ch_dict)
+    time = np.linspace(0, file_duration, file_duration * 200)
+    signals.append(np.sin(2 * np.pi * 8 * time))
+
+    ch_dict = {'label': 'sine 8.1777 Hz', 'dimension': 'uV', 'sample_rate': 200, 'physical_max': 100,
+               'physical_min': -100, 'digital_max': 32767, 'digital_min': -32768, 'transducer': '', 'prefilter': ''}
+    signal_headers.append(ch_dict)
+    time = np.linspace(0, file_duration, file_duration * 200)
+    signals.append(np.sin(2 * np.pi * 8.1777 * time))
+
+    ch_dict = {'label': 'sine 8.5 Hz', 'dimension': 'uV', 'sample_rate': 200, 'physical_max': 100,
+               'physical_min': -100, 'digital_max': 32767, 'digital_min': -32768, 'transducer': '', 'prefilter': ''}
+    signal_headers.append(ch_dict)
+    time = np.linspace(0, file_duration, file_duration * 200)
+    signals.append(np.sin(2 * np.pi * 8.5 * time))
+
+    ch_dict = {'label': 'sine 15 Hz', 'dimension': 'uV', 'sample_rate': 200, 'physical_max': 100,
+               'physical_min': -100, 'digital_max': 32767, 'digital_min': -32768, 'transducer': '', 'prefilter': ''}
+    signal_headers.append(ch_dict)
+    time = np.linspace(0, file_duration, file_duration * 200)
+    signals.append(np.sin(2 * np.pi * 15 * time))
+
+    ch_dict = {'label': 'sine 17 Hz', 'dimension': 'uV', 'sample_rate': 200, 'physical_max': 100,
+               'physical_min': -100, 'digital_max': 32767, 'digital_min': -32768, 'transducer': '', 'prefilter': ''}
+    signal_headers.append(ch_dict)
+    time = np.linspace(0, file_duration, file_duration * 200)
+    signals.append(np.sin(2 * np.pi * 17 * time))
+
+    ch_dict = {'label': 'sine 50 Hz', 'dimension': 'uV', 'sample_rate': 200.5, 'physical_max': 100,
+               'physical_min': -100, 'digital_max': 32767, 'digital_min': -32768, 'transducer': '', 'prefilter': ''}
+    signal_headers.append(ch_dict)
+    time = np.linspace(0, file_duration, int(file_duration * 200.5))
+    signals.append(np.sin(2 * np.pi * 50 * time))
+
+    pyedflib.highlevel.write_edf(test_data_file, signals, signal_headers, block_size=block_size)
