b214c822594f7d85abac09b8f5e97870b6fb278e
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2025-01-27 16:52:33 +0100

    Remove more code specific to Python 2

--- pyedflib/__init__.py
+++ pyedflib/__init__.py
@@ -16,9 +16,3 @@ from . import data
 from pyedflib.version import version as __version__
 
 __all__ = [s for s in dir() if not s.startswith('_')]
-try:
-    # In Python 2.x the name of the tempvar leaks out of the list
-    # comprehension.  Delete it to not make it show up in the main namespace.
-    del s
-except NameError:
-    pass
