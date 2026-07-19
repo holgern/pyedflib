be11fa3dcfe046d52fcdad6552ff96c2889e4d2f
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2026-07-19 15:10:09 +0200

    Drop support for Python 3.8
    Python 3.8 requires an obsolete version of pip that doesn't support PEP 639 and 735.
    
    Also ignore new default ruff rules so that CI tests pass.

--- setup.py
+++ setup.py
@@ -304,7 +304,6 @@ if __name__ == '__main__':
             "Programming Language :: C",
             "Programming Language :: Python",
             "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
