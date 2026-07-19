4c7c9dac3369f2a4061524a7ec1544f26432cade
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2026-07-19 18:12:59 +0200

    Apply ruff/Pylint rule PLR1730
    Replace `if` statement with `max(...)`

--- demo/streamingEDFFile.py
+++ demo/streamingEDFFile.py
@@ -24,8 +24,7 @@ if __name__ == '__main__':
     sigbufs = [np.zeros(f.getNSamples()[i]) for i in np.arange(n)]
     for i in np.arange(n):
         sigbufs[i] = f.readSignal(i)
-        if n_min < len(sigbufs[i]):
-            n_min = len(sigbufs[i])
+        n_min = max(n_min, len(sigbufs[i]))
 
     duration = f.getFileDuration()
     f._close()
